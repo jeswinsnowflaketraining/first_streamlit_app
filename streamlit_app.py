51d8ff7a651a80e2998da0618cec1e311931f5ee
jeswinsnowflaketraining <110232554+jeswinsnowflaketraining@users.noreply.github.com>
2022-07-29 11:52:33 +0100

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My parents are healthy diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text('Omega 2 and Blueteal')
+streamlit.text('Kale and Spinach')
+streamlit.text('Eggs and curry')
